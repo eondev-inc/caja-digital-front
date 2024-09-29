c57b211a94838031a24252b523ef497b73e6ebad
Yerffrey <eondev.inc@gmail.com>
2024-09-29 18:10:28 -0300

    :beers: new components, login and register almost complete

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -25,9 +25,9 @@ export const Register = () => {
   return(
     <section className="dark:bg-gray-900">
       <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
-        <div className="w-full rounded-lg border-slate-400 bg-white shadow-xl sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
+        <div className="w-full rounded-lg border-slate-400 bg-white shadow-xl dark:border dark:border-gray-700 dark:bg-gray-800 sm:max-w-md md:mt-0 xl:p-0">
           <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
                 Create an account
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
@@ -133,20 +133,20 @@ export const Register = () => {
                 <div className="flex h-5 items-center">
                   <Checkbox id="terms" name="terms" aria-describedby="terms"
                     {...register('checkBox')}
-                    className="focus:ring-primary-300 dark:focus:ring-primary-600 size-4 rounded 
-                    border border-gray-300 bg-gray-50 
-                    dark:border-gray-600 dark:bg-gray-700 
-                    dark:ring-offset-gray-800" 
+                    className="size-4 rounded border border-gray-300 
+                    bg-gray-50 focus:ring-primary-300 dark:border-gray-600 
+                    dark:bg-gray-700 dark:ring-offset-gray-800 
+                    dark:focus:ring-primary-600" 
                   />
                 </div>
                 <div className="ml-3 text-sm">
                   <Label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300" value="I accept the "/>
-                  <a className="text-primary-600 dark:text-primary-500 font-medium hover:underline" href="#">Terms and Conditions</a>
+                  <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a>
                 </div>
               </div>
-              <Button color="blue" type="submit" className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4">Create an account</Button>
+              <Button color="blue" type="submit" className="w-full rounded-lg bg-primary-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</Button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Already have an account? <a href="/login" className="text-primary-600 dark:text-primary-500 font-medium hover:underline">Login here</a>
+                Already have an account? <a href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
               </p>
             </form>
           </div>
