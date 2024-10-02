2ca8d2322d8e6a274053eba6c63c5923e612d992
seventrust-lite <eondev-inc@gmail.com>
2024-10-02 09:53:16 -0300

    :sparkles: updating some components

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,6 @@ import { login } from "../api/auth/login.post";
 import { useStore } from "../app/store";
 import { GeneralModal } from "../components/Commons/GeneralModal";
 import { useState } from "react";
-import { useEffect } from "react";
 
 export const Login = () => {
   const [show, setShow] = useState(false);
@@ -39,9 +38,9 @@ export const Login = () => {
     <>
       <section className="dark:bg-gray-900">
         <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
-          <div className="w-full rounded-lg border border-slate-300 bg-white shadow-2xl sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
+          <div className="w-full rounded-lg border border-slate-300 bg-white shadow-2xl dark:border dark:border-gray-700 dark:bg-gray-800 sm:max-w-md md:mt-0 xl:p-0">
             <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
                   Sign in to your account
               </h1>
               <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
@@ -49,9 +48,9 @@ export const Login = () => {
                   <Label htmlFor="email" className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Your email</Label>
                   <TextInput type="email" name="email"
                     {...register('email')}
-                    id="email" className="focus:border-primary-600 focus:ring-primary-600 
-                    block w-full 
-                    text-gray-900 dark:border-gray-600 
+                    id="email" className="block w-full 
+                    text-gray-900 focus:border-primary-600 
+                    focus:ring-primary-600 dark:border-gray-600 
                     dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 
                     dark:focus:border-blue-500 dark:focus:ring-blue-500" 
                     placeholder="name@company.com"
@@ -63,8 +62,8 @@ export const Login = () => {
                   <TextInput type="password" name="password" 
                     {...register('password')}
                     id="password" placeholder="••••••••" 
-                    className="focus:border-primary-600 focus:ring-primary-600  block
-                    w-full text-gray-900 
+                    className="block w-full  text-gray-900
+                    focus:border-primary-600 focus:ring-primary-600 
                     dark:border-gray-600 dark:bg-gray-700 
                     dark:text-white dark:placeholder:text-gray-400 
                     dark:focus:border-blue-500 dark:focus:ring-blue-500"
@@ -75,28 +74,28 @@ export const Login = () => {
                   <div className="flex items-start">
                     <div className="flex h-5 items-center">
                       <Checkbox id="remember" aria-describedby="remember" type="checkbox" 
-                        className="focus:ring-primary-300 dark:focus:ring-primary-600 
-                        dark:border-gray-600 dark:bg-gray-700 
-                        dark:ring-offset-gray-800"
+                        className="focus:ring-primary-300 dark:border-gray-600 
+                        dark:bg-gray-700 dark:ring-offset-gray-800 
+                        dark:focus:ring-primary-600"
                       />
                     </div>
                     <div className="ml-3 text-sm">
                       <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
                     </div>
                   </div>
-                  <a href="#" className="text-primary-600 dark:text-primary-500 text-sm font-medium hover:underline">Forgot password?</a>
+                  <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                 </div>
                 <Button type="submit"
-                  className="hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 
-                  dark:focus:ring-primary-800 w-full 
-                  text-center text-sm 
-                  font-medium text-white 
-                  focus:outline-none focus:ring-4"
+                  className="w-full text-center text-sm font-medium 
+                  text-white hover:bg-primary-700 
+                  focus:outline-none focus:ring-4 
+                  focus:ring-primary-300 dark:bg-primary-600 
+                  dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
                   Sign in
                 </Button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                    Don’t have an account yet? <a href="/register" className="text-primary-600 dark:text-primary-500 font-medium hover:underline">Sign up</a>
+                    Don’t have an account yet? <a href="/register" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                 </p>
               </form>
             </div>
@@ -104,13 +103,13 @@ export const Login = () => {
         </div>
       </section>
       <GeneralModal
-        title={error.error}
+        title={error ? error.error : "Error"}
         show={show} 
         typeModal={"error"} 
-        buttonMessage={error.message} 
+        buttonMessage={" Ok "} 
         onClose={handleModalClose}
       >
-        <p> An error has ocurred, please retry!</p>
+        <p> {error ? error.message : "Mensaje de error"}</p>
       </GeneralModal>
     </>
   )
--- src/pages/dashboard/Main.jsx
+++ src/pages/dashboard/Main.jsx
@@ -1,60 +1,70 @@
 import { Card } from "flowbite-react";
+import { useNavigate } from "react-router-dom";
 
 
 const Main = () => {
+  const navigateTo = useNavigate()
   const dashboardOptions = [
     {
       title: "Open Register",
       description: "Open a new cash register for your store",
       imgSrc: "img/cash-register.jpg",
-      link: "/open-register"
+      link: "./open-register"
     },
     {
       title: "Close Register",
       description: "Close an existing cash register for your store",
       imgSrc: "img/cash-register.jpg",
-      link: "/close-register"
+      link: "./close-register"
     },
     {
       title: "Sales",
       description: "View sales data for your store",
       imgSrc: "img/cash-register.jpg",
-      link: "/sales"
+      link: "./sales"
     },
     {
       title: "Anullments",
       description: "Manage products for your store",
       imgSrc: "img/cash-register.jpg",
-      link: "/anullments"
+      link: "./anullments"
     },
     {
       title: "Customers",
       description: "Manage customers for your store",
       imgSrc: "img/cash-register.jpg",
-      link: "/customers"
+      link: "./customers"
     },
     {
       title: "Settings",
       description: "Manage settings for your store",
       imgSrc: "img/cash-register.jpg",
-      link: "/settings"
+      link: "./settings"
     }
   ]
+
+  const goTo = (route) => {
+    navigateTo(route)
+  }
   return (
     <section>
       <div className="container mx-auto mt-5 p-4">
         <div className="-mx-2 flex flex-wrap">
-          {[...dashboardOptions].map((_, index) => (
-            <Card className="m-3 max-w-sm cursor-pointer" imgSrc="img/cash-register.jpg" horizontal key={index} >
+          { [...dashboardOptions].map((element, index) => (
+            <Card 
+              className="m-3 w-[460px] max-w-sm cursor-pointer" 
+              imgSrc="img/cash-register.jpg" horizontal key={index} 
+              onClick={() => goTo(element.link)}
+            >
               <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                { _.title }
+                { element.title }
               </h5>
               <p className="font-normal text-gray-700 dark:text-gray-400">
-                { _.description }
+                { element.description }
               </p>
 
             </Card>
-          ))}
+          )) }
         </div>
       </div>
     </section>
